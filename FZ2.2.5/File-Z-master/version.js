bf22c2d46df74779c02ca9e2db8645912556a559
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2023-09-24 20:52:07 -0700

    2.2.5!

--- FZ2.2.5/File-Z-master/version.js
+++ FZ2.2.5/File-Z-master/version.js
@@ -1,4 +1,4 @@
-window.localStorage.setItem("siteVersion", "2.2.5 Beta"); // set site version
+window.localStorage.setItem("siteVersion", "2.2.5"); // set site version
 var siteVersion = window.localStorage.getItem("siteVersion");
 
 //toast
